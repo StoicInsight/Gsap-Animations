d644dac54751de561cb3bb00bd40d483c049111c
stoicinsight <65826912+StoicInsight@users.noreply.github.com>
2023-05-11 22:46:32 -0400

    animate items class

--- Timelines/main.js
+++ Timelines/main.js
@@ -9,3 +9,45 @@ const circle2 = document.querySelector(".circle2");
 gsap.to(circle1, {x: 300, duration: 3, scale: 2, rotation: 360, ease: "power2.inOut"});
 
 gsap.from(circle2, {x: 400, y:200, scale: 3, rotation: 360, duration: 3, ease: "power2.inOut"});
+
+class animateButton {
+  constructor(btn, item) {
+      this.btn = document.querySelector(btn);
+  }
+
+  tween() {
+    gsap.to(this.btn, {
+      duration: 4,
+      x: () => buttons.offsetWidth,
+      xPercent: -100,
+      rotation: 360,
+      ease: "none",
+      paused: true
+    })
+  }
+
+  play() {
+    this.btn.onClick = () => this.tween.play()
+  }
+
+  pause() {
+    this.btn.onClick = () => this.tween.pause()
+
+  } 
+
+  restart() {
+    this.btn.onClick = () => this.tween.restart()
+
+  }
+
+  resume() {
+    this.btn.onClick = () => this.tween.resume()
+
+  }
+
+  reverse() {
+    this.btn.onClick = () => this.tween.reverse()
+  }
+
+
+}
