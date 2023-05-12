f76c6645cfc117d0f5707d0e77f91445fba17339
stoicinsight <65826912+StoicInsight@users.noreply.github.com>
2023-05-12 11:14:19 -0400

    scroll progress

--- scrollTrigger/main.js
+++ scrollTrigger/main.js
@@ -14,3 +14,53 @@ ScrollTrigger.create({
   pin: true,
   scrub: 0.6
 })
+
+let lastTime = 0
+
+function throttle(func, timeFrame) {
+  return function () {
+    let now = Date.now();
+    if(now - lastTime >= timeFrame) {
+      func()
+      lastTime = now
+    }
+  }
+}
+
+let currentSection = 0;
+let prevSection = currentSection;
+
+const dots = document.querySelectorAll('.dot')
+const progressEl = document.querySelector('.footer-progress')
+
+window.addEventListener('whell', (e) => {
+  const callBack = () => {
+    let delta = e.deltaY;
+    prevSection = currentSection;
+    if(delta > 0) {
+      if(currentSection < dots.lenght) {
+        currentSection += 1;
+        jumpToSection()
+      }
+    } else {
+      if(currentSection > 0) {
+        currentSection -= 1;
+        jumpToSection()
+      }
+    }
+  }
+
+  let t = throttle(callBack, 500)
+  t()
+})
+
+const jumpToSection = () => {
+  setProgress(currentSection)
+}
+
+const setProgress = () => {
+	dots[prevSection].classList.remove("active");
+  dots[currentSection].classList.add("active");
+  const percent = (currentSection / (dots.length - 1)) * 100;
+  progressEl.style.width = percent + "%";
+};
