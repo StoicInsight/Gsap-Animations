2f6b5e2473aa85804d54f5a9dee84f4bd1e5dfd0
stoicinsight <65826912+StoicInsight@users.noreply.github.com>
2023-05-11 21:51:19 -0400

    gsap from tween

--- Timelines/main.js
+++ Timelines/main.js
@@ -1,5 +1,11 @@
 import { gsap } from "gsap";
 
 const box = document.querySelector(".box");
+const circle1 = document.querySelector(".circle1");
+const circle2 = document.querySelector(".circle2");
 
-gsap.to(box, {x: 300, duration: 4})
+
+// Scale img while moving to the x axis
+gsap.to(circle1, {x: 300, duration: 3, scale: 2, rotation: 360, ease: "power2.inOut"});
+
+gsap.from(circle2, {x: 400, y:200, scale: 3, rotation: 360, duration: 3, ease: "power2.inOut"});
